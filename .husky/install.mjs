d215b5296e16675ca4ed3dbe8b1d396123d87b14
victorokonkwo67@gmail.com <victorokonkwo67@gmail.com>
2024-09-17 03:21:37 +0100

    fix: resolve husky issues in prod

--- .husky/install.mjs
+++ .husky/install.mjs
@@ -1,7 +1,7 @@
 // Skip Husky install in production and CI
 if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
   console.log('Skipping Husky install in production or CI.');
-  return;
+  process.exit(0);
 }
 const husky = (await import('husky')).default;
 console.log(husky());
